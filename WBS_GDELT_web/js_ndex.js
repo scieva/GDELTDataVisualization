32d5aaa3690411882ca9ef20c14f7fa976388cd7
scieva <38570213+scieva@users.noreply.github.com>
2021-03-08 21:28:40 +0100

    Innitial commit

---
+++ WBS_GDELT_web/js_ndex.js
@@ -0,0 +1,29 @@
+function noneHandler(){
+    var img = document.getElementById("mapa");
+    img.src = "C:\\Users\\sciev\\OneDrive\\Desktop\\PyCharm Projects\\WBS_GDELT\\Backup images\\none.png"
+}
+
+function allHandler(){
+    var img = document.getElementById("mapa");
+    img.src = "C:\\Users\\sciev\\OneDrive\\Desktop\\PyCharm Projects\\WBS_GDELT\\Backup images\\all.png"
+}
+
+function allLHandler(){
+    var img = document.getElementById("mapa");
+    img.src = "C:\\Users\\sciev\\OneDrive\\Desktop\\PyCharm Projects\\WBS_GDELT\\Backup images\\all_lines.png"
+}
+
+function chnHandler() {
+    var img = document.getElementById("mapa");
+    img.src = "C:\\Users\\sciev\\OneDrive\\Desktop\\PyCharm Projects\\WBS_GDELT\\Backup images\\chn.png"
+}
+
+function rusHandler() {
+    var img = document.getElementById("mapa");
+    img.src = "C:\\Users\\sciev\\OneDrive\\Desktop\\PyCharm Projects\\WBS_GDELT\\Backup images\\rus.png"
+}
+
+function usaHandler() {
+    var img = document.getElementById("mapa");
+    img.src = "C:\\Users\\sciev\\OneDrive\\Desktop\\PyCharm Projects\\WBS_GDELT\\Backup images\\usa.png"
+}
