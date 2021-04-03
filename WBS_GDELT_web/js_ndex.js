7033b5b08ed1bf598813a31140aec54feecd3a39
scieva <38570213+scieva@users.noreply.github.com>
2021-04-03 21:12:50 +0200

    Added D3 diagram

--- WBS_GDELT_web/js_ndex.js
+++ WBS_GDELT_web/js_ndex.js
@@ -1,29 +1,47 @@
 function noneHandler(){
     var img = document.getElementById("mapa");
     img.src = "none.png"
+    img.style.display = 'block'
+    document.getElementById('d3').style.display = 'none'
 }
 
 function allHandler(){
     var img = document.getElementById("mapa");
     img.src = "all.png"
+    img.style.display = 'block'
+    document.getElementById('d3').style.display = 'none'
 }
 
 function allLHandler(){
     var img = document.getElementById("mapa");
     img.src = "all_lines.png"
+    img.style.display = 'block'
+    document.getElementById('d3').style.display = 'none'
 }
 
 function chnHandler() {
     var img = document.getElementById("mapa");
     img.src = "chn.png"
+    img.style.display = 'block'
+    document.getElementById('d3').style.display = 'none'
 }
 
 function rusHandler() {
     var img = document.getElementById("mapa");
     img.src = "rus.png"
+    img.style.display = 'block'
+    document.getElementById('d3').style.display = 'none'
 }
 
 function usaHandler() {
     var img = document.getElementById("mapa");
     img.src = "usa.png"
+    img.style.display = 'block'
+    document.getElementById('d3').style.display = 'none'
+}
+
+function hideImg() {
+    var img = document.getElementById("mapa");
+    img.style.display = 'none'
+    document.getElementById('d3').style.display = 'block'
 }
